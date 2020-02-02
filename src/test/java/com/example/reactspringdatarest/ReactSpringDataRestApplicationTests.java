bae2d03958283b6db5770f4cef08a417a7c0a590
paromaniuk <paromaniuk@grupainteger.pl>
2020-02-02 11:17:09 +0100

    setup spring project

---
+++ src/test/java/com/example/reactspringdatarest/ReactSpringDataRestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.reactspringdatarest;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
